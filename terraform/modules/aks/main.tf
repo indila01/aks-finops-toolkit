219f81000b62daad8bd1c830ed30b66f0b595ab6
indila <indiladineth@yahoo.com>
2026-03-01 17:17:27 +0530

    fix: revert auto_scaling_enabled to enable_auto_scaling for azurerm ~> 3.90
    auto_scaling_enabled is the azurerm v4 attribute name. The provider constraint
    is ~> 3.90 which uses enable_auto_scaling. Also fixes fmt alignment in the
    monitoring node pool block after the attribute rename.

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -21,14 +21,14 @@ resource "azurerm_kubernetes_cluster" "this" {
   kubernetes_version  = var.kubernetes_version
 
   default_node_pool {
-    name                 = "system"
-    node_count           = var.system_node_count
-    vm_size              = var.system_node_vm_size
-    os_disk_size_gb      = 80
-    type                 = "VirtualMachineScaleSets"
-    auto_scaling_enabled = true
-    min_count            = 1
-    max_count            = var.system_node_count + 2
+    name                = "system"
+    node_count          = var.system_node_count
+    vm_size             = var.system_node_vm_size
+    os_disk_size_gb     = 80
+    type                = "VirtualMachineScaleSets"
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = var.system_node_count + 2
 
     upgrade_settings {
       max_surge = "10%"
@@ -65,7 +65,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "monitoring" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
   vm_size               = var.monitoring_node_vm_size
   node_count            = 1
-  auto_scaling_enabled  = false
+  enable_auto_scaling   = false
   os_disk_size_gb       = 60
 
   node_labels = {
